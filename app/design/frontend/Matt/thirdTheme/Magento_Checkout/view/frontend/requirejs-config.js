dc0f4a10eba1f11a3b429787822d5dca93e44938
matviyyakunin <m.yakunin@mavenecommerce.com>
2020-01-10 12:35:30 +0200

    third exercise almost done

--- app/design/frontend/Matt/thirdTheme/Magento_Checkout/view/frontend/requirejs-config.js
+++ app/design/frontend/Matt/thirdTheme/Magento_Checkout/view/frontend/requirejs-config.js
@@ -1,8 +1,8 @@
-
 var config = {
     map: {
         '*': {
-            'Magento_Checkout/template/form/element/email.html': 'Magento_Checkout/template/form/element/email.html'
+            'Magento_Checkout/template/form/element/email.html': 'Magento_Checkout/template/form/element/email.html',
+            'Magento_Checkout/js/index.js':'Magento_Checkout/js/index.js',
         }
     }
 };
---
+++ app/design/frontend/Matt/thirdTheme/Magento_Checkout/view/frontend/web/js/index.js
@@ -0,0 +1,7 @@
+define(['jquery'], function($){
+    "use strict";
+    return function hello()
+    {
+        console.log('Bonjour Amir');
+    }
+});
