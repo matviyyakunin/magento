2b79bba4dbcfd40d0b886c224746ec99b4cb0726
matviyyakunin <m.yakunin@mavenecommerce.com>
2020-01-13 09:42:55 +0200

    monday morning

--- app/design/frontend/Matt/thirdTheme/Magento_Checkout/view/frontend/requirejs-config.js
+++ app/design/frontend/Matt/thirdTheme/Magento_Checkout/view/frontend/requirejs-config.js
@@ -2,7 +2,6 @@ var config = {
     map: {
         '*': {
             'Magento_Checkout/template/form/element/email.html': 'Magento_Checkout/template/form/element/email.html',
-            'Magento_Checkout/js/index.js':'Magento_Checkout/js/index.js',
         }
     }
 };
