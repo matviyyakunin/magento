696d1f1851dc43cae955c8cd49fdd6dfd9a15f87
matviyyakunin <m.yakunin@mavenecommerce.com>
2020-01-22 14:06:15 +0200

    fifth 60%

--- app/design/frontend/Matt/thirdTheme/Magento_Search/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-// var config = {
-//     "map": {
-//         "*": {
-//             "popup": "js/search-popup",
-//         }
-//     }
-// };
--- app/design/frontend/Matt/thirdTheme/Magento_Search/web/js/search-popup.js
+++ app/design/frontend/Matt/thirdTheme/Magento_Search/web/js/search-popup.js
@@ -1,10 +1,9 @@
+
 define([
     'jquery',
     'Magento_Ui/js/modal/modal'
 ], function($, modal) {
 return function (config, element) {
-    console.log(config);
-    console.log(element);
     let options = {
         type: 'popup',
         responsive: true,
@@ -12,16 +11,28 @@ return function (config, element) {
         buttons: [{
             class: 'close-search-popup',
             click: function() {
-                this.closeModal();
+                const value = $('#custom-input')[0].value; // getting information from input
+                function validateEmail(email) { //function for  checking  values from input and returning true or false
+                    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    return re.test(String(email).toLowerCase());
+                }
+                let checkResult = validateEmail(value);// variable for result  containing of validateEmail function
+                 if(checkResult){// condition to allow close modal window and save data either localStorage or cookies
+                    this.closeModal();
+                    localStorage.setItem('email',`${value}`);
+                }
+                 else{
+                 }
             }
         }],
+
         opened: function() {
             $('#search').focus();
         }
     };
     let popup = modal(options, $('#search-block'));
-    popup.openModal();
 
+    popup.openModal();
     $(element).on('click', function() {
          popup.openModal();
     });
